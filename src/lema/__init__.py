50e8e1d161893a79e265588a87df5e18494030aa
Yunare Maia <yunare@gmail.com>
2025-02-02 21:44:43 -0300

    Export lema entrypoint scripts (#3)
    - Small PR to make the main scripts available in the lema module
    `lema.train` & `lema.evaluate`.
    - Update project config to install `lema-train` & `lema-evaluate`
    scripts
    - Add placeholder `evaluate` script.

---
+++ src/lema/__init__.py
@@ -0,0 +1,4 @@
+from lema.evaluate import evaluate
+from lema.train import train
+
+__all__ = ["train", "evaluate"]
---
+++ src/lema/evaluate.py
@@ -0,0 +1,15 @@
+from lema.core.types import EvaluationConfig
+
+
+def main():
+    """Main entry point for evaluating LeMa."""
+    raise NotImplementedError
+
+
+def evaluate(config: EvaluationConfig) -> None:
+    """Evaluate a model using the provided configuration."""
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
